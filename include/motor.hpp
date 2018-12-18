ba2da09857c7f70f6ff6459021465e9c1e4e6f5b
JonathanPlasse <jonathan.plasse@live.fr>
2018-12-18 14:50:54 +0100

    add motor.hpp

---
+++ include/motor.hpp
@@ -0,0 +1,25 @@
+#ifndef MOTOR_HPP
+#define MOTOR_HPP
+
+class Motor {
+public:
+  //Initialisation of the Motor class
+  Motor(uint8_t dirPin1, uint8_t dirPin2, uint8_t pwmPin, Encoder enc);
+  //Set the pwm sent to the H bridge
+  void setPwm(int16_t pwm);
+  //Get the pwm sent to the H bridge
+  int16_t getPwm();
+  //Get the position of the motor in step
+  int32_t getPosition();
+  //Get the speed of the motor in step/second
+  //deltaT is the time since the last call of the function
+  int32_t getSpeed(uint32_t deltaT);
+
+private:
+  Encoder _enc;
+  uint8_t _dirPin1, _dirPin2, _pwmPin;
+  int16_t _pwm;
+  int32_t _position, _lastPosition;
+}
+
+#endif
