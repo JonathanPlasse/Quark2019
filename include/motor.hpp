9cf734b43f7187651f0ce9ba81c534a387479050
JonathanPlasse <jonathan.plasse@live.fr>
2019-02-07 13:48:20 +0100

    Start adding control system

--- include/motor.hpp
+++ include/motor.hpp
@@ -7,22 +7,27 @@
 class Motor {
 public:
   //Initialisation of the Motor class
-  Motor(uint8_t dirPin1, uint8_t dirPin2, uint8_t pwmPin, const Encoder& enc);
-  //Set the pwm sent to the H bridge
-  void setPwm(int16_t pwm);
+  Motor(uint8_t dirPin1, uint8_t dirPin2, uint8_t pwmPin, const Encoder& enc, uint8_t sampleTime);
   //Get the pwm sent to the H bridge
   int16_t getPwm();
+  //Set the pwm sent to the H bridge
+  void setPwm(int16_t pwm);
   //Get the position of the motor in step
   int32_t getPosition();
   //Get the speed of the motor in step/second
-  //deltaT is the time since the last call of the function
-  int32_t getSpeed(uint32_t deltaT);
+  int32_t getSpeed();
+  //Set the speed of the motor in step/s
+  void setSpeed(int16_t speed);
+  //Run the control system of the speed
+  void run();
 
 private:
   Encoder _enc;
   uint8_t _dirPin1, _dirPin2, _pwmPin;
   int16_t _pwm;
   int32_t _position, _lastPosition;
+  uint8_t _sampleTime;
+  int32_t _speed;
 };
 
 #endif
--- src/motor.cpp
+++ src/motor.cpp
@@ -1,20 +1,24 @@
 #include "motor.hpp"
 #include <Arduino.h>
 
-Motor::Motor(uint8_t dirPin1, uint8_t dirPin2, uint8_t pwmPin, const Encoder& enc) :
-_dirPin1(dirPin1), _dirPin2(dirPin2), _pwmPin(pwmPin) , _enc(enc) {
+Motor::Motor(uint8_t dirPin1, uint8_t dirPin2, uint8_t pwmPin, const Encoder& enc, uint8_t sampleTime) :
+_dirPin1(dirPin1), _dirPin2(dirPin2), _pwmPin(pwmPin) , _enc(enc), _sampleTime(sampleTime) {
   pinMode(_dirPin1, OUTPUT);
   pinMode(_dirPin2, OUTPUT);
   analogWrite(_pwmPin, 0);
 }
 
+int16_t Motor::getPwm() {
+  return _pwm;
+}
+
 void Motor::setPwm(int16_t pwm) {
-  if(pwm>0){
+  if (pwm > 0) {
     digitalWrite(_dirPin1, LOW);
     digitalWrite(_dirPin2, HIGH);
     analogWrite(_pwmPin, pwm);
   }
-  else{
+  else {
     digitalWrite(_dirPin1, HIGH);
     digitalWrite(_dirPin2, LOW);
     analogWrite(_pwmPin, -pwm);
@@ -22,16 +26,20 @@ void Motor::setPwm(int16_t pwm) {
   _pwm = pwm;
 }
 
-int16_t Motor::getPwm() {
-  return _pwm;
-}
-
 int32_t Motor::getPosition() {
   return _enc.read();
 }
 
-int32_t Motor::getSpeed(uint32_t deltaT){
+int32_t Motor::getSpeed() {
   _lastPosition = _position;
   _position = getPosition();
-  return (_position - _lastPosition) / deltaT;
+  return (_position - _lastPosition) / _sampleTime;
+}
+
+void Motor::setSpeed(int16_t speed) {
+  _speed = speed;
+}
+
+void Motor::run() {
+  //To implement
 }
