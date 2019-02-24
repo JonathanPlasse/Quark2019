fcdbc23d220b9772f08516b12e1a2b3bd9d150b0
JonathanPlasse <jonathan.plasse@live.fr>
2019-02-24 16:45:57 +0100

    Add multiple measures of the motor response

--- src/main.cpp
+++ src/main.cpp
@@ -25,35 +25,56 @@
 #define KD 0
 
 //Test Moteur
-#define MAX_TIME 1000
+#define MEASURE_TIME 500
+#define WAIT_TIME 500
+#define PWM 220
 
 Motor m1(M1_DIR1, M1_DIR2, M1_PWM, ENC1_PIN1, ENC1_PIN2, SAMPLE_TIME, KP, KI, KD);
 Motor m2(M2_DIR1, M2_DIR2, M2_PWM, ENC2_PIN1, ENC2_PIN2, SAMPLE_TIME, KP, KI, KD);
 
-uint32_t lastTime, time;
+uint32_t lastTime, time, startTime;
 float speed;
-int32_t sampleTime;
+uint16_t sampleTime, nbMeasure, nbMeasureDone;
 
 void setup() {
   /*Change the frequency of the pins 9, 10*/
   TCCR1B = (TCCR1B & 0xf8) | 0x01;
 
   Serial.begin(115200);
+
+  //Ask sample time
   sampleTime = Serial.parseInt();
-  lastTime = millis() - SAMPLE_TIME;
-  m1.setPwm(220);
+  m1.setSampleTime(sampleTime);
+
+  //Ask the number of measure to do
+  nbMeasure = Serial.parseInt();
+  nbMeasureDone = 0;
+
+  startTime = lastTime = millis() - SAMPLE_TIME;
+  m1.setPwm(PWM);
 }
 
 
 void loop() {
   time = millis();
-  if (time > MAX_TIME) {
-    m1.stop();
-  }
-  else if (time - lastTime > SAMPLE_TIME) {
+  if (nbMeasureDone < nbMeasure && time - lastTime > SAMPLE_TIME) {
     lastTime += SAMPLE_TIME;
     m1.computeSpeed();
-    speed = m1.getActualSpeed();
-    Serial.println(speed);
+
+    if (time - startTime < MEASURE_TIME) {
+      m1.computeSpeed();
+      speed = m1.getActualSpeed();
+      Serial.print(speed);
+      Serial.print(" ");
+    }
+    else if (time - startTime < MEASURE_TIME + WAIT_TIME) {
+      m1.stop();
+    }
+    else {
+      startTime = lastTime;
+      nbMeasureDone++;
+      Serial.println("");
+      m1.setPwm(PWM);
+    }
   }
 }
