592db62821784f59989cbd224bf8f4f7c6a3cc41
JonathanPlasse <jonathan.plasse@live.fr>
2019-03-13 16:45:45 +0100

    Test echo config

--- interface.py
+++ interface.py
@@ -1,32 +1,73 @@
 #!/usr/bin/env python3
 import serial
-import io
+import struct
 import time
 import matplotlib.pyplot as plt
 
-def echelon():
-    with serial.Serial('/dev/ttyACM0', 115200, timeout=2) as ser:
-        sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+typesDict = {'char': 'c', 'bool': '?',
+             'int8': 'b', 'uint8': 'B',
+             'int16': 'h', 'uint16': 'H',
+             'int32': 'i', 'uint32': 'I',
+             'int64': 'l', 'uint64': 'L',
+             'float': 'f'}
 
-        speeds = []
+def computeFormat(structFormat):
+    """Compute the format string for struct.(pack/unpack)"""
+    structTypes = '='
 
-        lastTime = time.time()
-        while time.time()-lastTime < 2:
-            speed = sio.readline().strip()
-            if speed != '':
-                lastTime = time.time()
-                # print(float(speed))
-                speeds.append(speed)
-    return speeds
+    for t in structFormat:
+        structTypes += typesDict[t]
+
+    return structTypes
+
+def readData(ser, structFormat):
+    structTypes = computeFormat(structFormat)
+    nbBytes = struct.calcsize(structTypes)
+    # Wait until all the data is in the buffer
+    while ser.in_waiting < nbBytes:
+        pass
+    # Read the raw data
+    rawData = bytearray(nbBytes)
+    ser.readinto(rawData)
+    # Convert the raw data
+    data = list(struct.unpack(structTypes, rawData))
+    return data
+
+def writeData(ser, structFormat, data):
+    structTypes = computeFormat(structFormat)
+    rawData = struct.pack(structTypes, *data)
+    ser.write(rawData)
 
 if __name__ == '__main__':
-    speeds = echelon()
-    with open("speeds.txt", "a+") as f:
-        for speed in speeds:
-            f.write(speed+"\n")
+    portName = '/dev/ttyACM0'
+    baudRate = 115200
 
-    t = [i*0.001 for i in range(len(speeds))]
-    plt.plot(t, speeds)
-    plt.xlabel("Time in seconds")
-    plt.ylabel("Speed in steps/seconds")
-    plt.show()
+    nbStepMeasure = 1
+    nbSample = 100
+    measureTime = 1000
+    waitTime = 500
+    pwm = 220
+
+    # Define the format of the structure of data sent
+    structFormatConfig = ['uint8', 'uint16', 'uint16', 'uint16', 'uint8']
+    structFormatMeasure = ['uint32', 'uint32', 'float']
+    structFormatSize = ['uint32', 'uint32']
+
+    # speeds = [[0. for j in range(nbSample)] for i in range(nbStepMeasure)]
+
+    with serial.Serial(portName, baudRate, timeout=1) as ser:
+        # Wait for the arduino to initilize
+        time.sleep(2)
+        # Write some data to the arduino
+        writeData(ser, structFormatConfig, [nbStepMeasure, nbSample, measureTime, waitTime, pwm])
+        print(readData(ser, structFormatConfig))
+        # for i in range(len(nbStepMeasure)):
+        #     for j in range(len(nbSample)):
+        #         speeds[i][j] = readData(ser, structFormatMeasure)
+        # print(speeds)
+
+    # t = [i*0.001 for i in range(len(speeds))]
+    # plt.plot(t, speeds)
+    # plt.xlabel("Time in seconds")
+    # plt.ylabel("Speed in steps/seconds")
+    # plt.show()
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Encoder.h>
 #include "motor.hpp"
 #include "conversion.h"
+#include "binary_serial.hpp"
 
 //Moteur Gauche
 #define M1_DIR1 A0
@@ -24,42 +25,44 @@
 #define KI 0
 #define KD 0
 
-//Test Moteur
-#define MEASURE_TIME 500
-#define PWM 220
+typedef struct {uint8_t nbStepMeasure; uint16_t nbSample; uint16_t measureTime; uint16_t waitTime; uint8_t pwm;} config;
+typedef struct {uint32_t timestamp; uint32_t position; float vitesse;} measure;
+
+config configPy;
+measure measurePy;
 
 Motor m1(M1_DIR1, M1_DIR2, M1_PWM, ENC1_PIN1, ENC1_PIN2, SAMPLE_TIME, KP, KI, KD);
 Motor m2(M2_DIR1, M2_DIR2, M2_PWM, ENC2_PIN1, ENC2_PIN2, SAMPLE_TIME, KP, KI, KD);
 
 uint32_t lastTime, time, startTime;
-float speed;
-uint16_t sampleTime, nbMeasure, nbMeasureDone;
 
 void setup() {
   /*Change the frequency of the pins 9, 10*/
   TCCR1B = (TCCR1B & 0xf8) | 0x01;
 
   Serial.begin(115200);
+  readData(&configPy, sizeof(config));
+  writeData(&configPy, sizeof(config));
 
-  startTime = lastTime = millis() - SAMPLE_TIME;
-  m1.setPwm(PWM);
+  // startTime = lastTime = millis() - SAMPLE_TIME;
+  // m1.setPwm(configPy.pwm);
 }
 
 
 void loop() {
-  time = millis();
-  if (time - lastTime > SAMPLE_TIME) {
-    lastTime += SAMPLE_TIME;
-    m1.computeSpeed();
-
-    if (time - startTime < MEASURE_TIME) {
-      speed = m1.getActualSpeed();
-      Serial.print(speed);
-      Serial.print(" ");
-    }
-    else {
-      m1.stop();
-      Serial.println("");
-    }
-  }
+  // time = millis();
+  // if (time - lastTime > SAMPLE_TIME) {
+  //   lastTime += SAMPLE_TIME;
+  //   m1.computeSpeed();
+  //
+  //   if (time - startTime < MEASURE_TIME) {
+  //     speed = m1.getActualSpeed();
+  //     Serial.print(speed);
+  //     Serial.print(" ");
+  //   }
+  //   else {
+  //     m1.stop();
+  //     Serial.println("");
+  //   }
+  // }
 }
