9fc8f0cb35d80b71afca6d134eaf91c7d7dbb217
JonathanPlasse <jonathan.plasse@live.fr>
2019-03-23 18:19:02 +0100

    only one motor modelisation

--- motor_caract.py
+++ motor_caract.py
@@ -10,29 +10,15 @@ def f(t, k, tau):
 def residual(p, t, speed):
     return speed - f(t, *p)
 
-speed1 = np.loadtxt('speed1.csv')
-speed2 = np.loadtxt('speed2.csv')
+speed = np.loadtxt('speed1.csv')
 
 p0 = np.ones(2)
 
 t = np.array([0.01 * i for i in range(100)])
 
-p1, _ = leastsq(residual, p0, args=(t, speed1))
-p2, _ = leastsq(residual, p0, args=(t, speed2))
+p, _ = leastsq(residual, p0, args=(t, speed))
 
-print(p1)
-print(p2)
-
-plt.figure(1)
-plt.plot(t, speed1, label='real speed1')
-plt.plot(t, f(t, *p1), label='model speed1')
-plt.plot(t, speed2, label='real speed2')
-plt.plot(t, f(t, *p2), label='model speed2')
-plt.legend()
-plt.show()
-
-k1, tau1 = p1
-k2, tau2 = p2
+k1, tau1 = p
 
 nbStep = 1633
 pwm = 220
@@ -42,29 +28,30 @@ ki = 752
 
 c = cnt.tf([kp, ki*Ts-kp], [1, -1], Ts)
 
-g1d = cnt.tf(k1 / nbStep / pwm, [tau1, 1]).sample(Ts)
-g2d = cnt.tf(k2 / nbStep / pwm, [tau2, 1]).sample(Ts)
+gd = cnt.tf(k1 / nbStep / pwm, [tau1, 1]).sample(Ts)
 
-y1 = cnt.feedback(c*g1d)
-u1 = cnt.feedback(c, g1d)
-y2 = cnt.feedback(c*g2d)
-u2 = cnt.feedback(c, g2d)
+y = cnt.feedback(c*gd)
+u = cnt.feedback(c, gd)
 
 t = [0.01 * i for i in range(100)]
 
-_, stepY1 = cnt.step_response(y1, t)
-_, stepY2 = cnt.step_response(y2, t)
-_, stepU1 = cnt.step_response(u1, t)
-_, stepU2 = cnt.step_response(u2, t)
+_, stepY = cnt.step_response(y, t)
+_, stepU = cnt.step_response(u, t)
 
-plt.subplot(1, 2, 1)
-plt.plot(t, stepY1[0], label='y1')
-plt.plot(t, stepY2[0], label='y2')
-plt.title('y response')
+plt.subplot(131)
+plt.plot(t, speed, label='real speed')
+plt.plot(t, f(t, *p), label='model speed')
+plt.title('Open-loop')
+plt.legend()
 
-plt.subplot(1, 2, 2)
-plt.plot(t, stepU1[0], label='u1')
-plt.plot(t, stepU2[0], label='u2')
-plt.title('u response')
+plt.subplot(132)
+plt.plot(t, stepY[0], label='Y')
+plt.title('Y response')
+plt.legend()
+
+plt.subplot(133)
+plt.plot(t, stepU[0], label='U')
+plt.title('U response')
+plt.legend()
 
 plt.show()
