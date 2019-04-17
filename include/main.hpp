7b21e002d532498c9ebdad6e230441ce2d941fd6
Jonathan Plasse <jonathan.plasse@live.fr>
2019-04-17 21:09:51 +0200

    Move define and typedef to hpp

--- include/main.hpp
+++ include/main.hpp
@@ -3,7 +3,40 @@
 
 #include <Encoder.h>
 #include "motor.hpp"
-#include "rst.hpp"
+
+// Left Motor
+#define M1_DIR1 A0
+#define M1_DIR2 A1
+#define M1_PWM 9
+
+// Left Encoder
+#define ENC1_PIN1 2
+#define ENC1_PIN2 4
+
+// Right Motor
+#define M2_DIR1 A2
+#define M2_DIR2 A3
+#define M2_PWM 10
+
+// Right Encoder
+#define ENC2_PIN1 3
+#define ENC2_PIN2 5
+
+// Configuration structure for step response
+typedef struct {
+  uint8_t pwm;
+  uint8_t sample_time;
+  uint8_t nb_measure;
+  uint16_t nb_sample;
+  uint16_t wait_time;
+} configStruct;
+
+// Motor measure
+typedef struct {
+  uint32_t timestamp;
+  uint32_t position;
+  float speed;
+} measureStruct;
 
 // Execute function every sample_time
 void timer(uint32_t time, uint32_t sample_time);
--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,7 @@
 #include <Arduino.h>
 #include <stdint.h>
 #include "binary_serial.hpp"
-
-// Left Motor
-#define M1_DIR1 A0
-#define M1_DIR2 A1
-#define M1_PWM 9
-
-// Left Encoder
-#define ENC1_PIN1 2
-#define ENC1_PIN2 4
-
-// Right Motor
-#define M2_DIR1 A2
-#define M2_DIR2 A3
-#define M2_PWM 10
-
-// Right Encoder
-#define ENC2_PIN1 3
-#define ENC2_PIN2 5
-
-// Configuration structure for step response
-typedef struct {
-  uint8_t pwm;
-  uint8_t sample_time;
-  uint8_t nb_measure;
-  uint16_t nb_sample;
-  uint16_t wait_time;
-} configStruct;
-
-// Motor measure
-typedef struct {
-  uint32_t timestamp;
-  uint32_t position;
-  float speed;
-} measureStruct;
+#include "rst.hpp"
 
 // Initialization of the structure for communication
 configStruct config;
