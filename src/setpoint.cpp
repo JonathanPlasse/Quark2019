12c765d84f628eae6042c98cda21e7a426caf0f8
Jonathan Plasse <jonathan.plasse@live.fr>
2019-04-22 16:33:05 +0200

    start implement Setpoint.update()

--- src/setpoint.cpp
+++ src/setpoint.cpp
@@ -1,4 +1,5 @@
 #include "setpoint.hpp"
+#include <math.h>
 
 Setpoint::Setpoint(float* left_control, float* right_control) :
 _left_control(left_control), _right_control(right_control) {}
@@ -12,5 +13,9 @@ void Setpoint::set_setpoint_position(const position_t* setpoint_position) {
 }
 
 void Setpoint::update() {
-
+  float delta_x = _setpoint_position->x - _current_position->x;
+  float delta_y = _setpoint_position->y - _current_position->y;
+  float delta_theta = _setpoint_position->theta - _current_position->theta;
+  float delta_translation = sqrt(delta_x*delta_x + delta_y*delta_y);
+  float delta_rotation = atan2f(delta_y, delta_x);
 }
