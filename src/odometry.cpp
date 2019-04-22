1d2996a3b26a019fe3963ff9540825c652c47685
Jonathan Plasse <jonathan.plasse@live.fr>
2019-04-22 17:11:08 +0200

    use step2cm

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -4,8 +4,8 @@
 Odometry::Odometry() : _position({0, 0, 0}) {}
 
 void Odometry::update(float left_step, float right_step) {
-  float left_distance = left_step * wheel_perimeter / resolution;
-  float right_distance = right_step * wheel_ratio * wheel_perimeter / resolution;
+  float left_distance = step2cm(left_step);
+  float right_distance = step2cm(right_step) * wheel_ratio;
 
   float translation = (left_distance + right_distance) / 2;
   float rotation = (-left_distance + right_distance) / center_distance;
