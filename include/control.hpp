3df3123757e5563b7548ef01887fc50db6034116
Jonathan Plasse <jonathan.plasse@live.fr>
2019-05-18 16:17:58 +0200

    remove speed function

--- include/control.hpp
+++ include/control.hpp
@@ -13,6 +13,4 @@ typedef struct {
   float delta_rotation;
 } delta_move_t;
 
-float speed(const control_t* control, const control_t* last_control, float deltat_t);
-
 #endif
--- src/control.cpp
+++
@@ -1,5 +0,0 @@
-#include "control.hpp"
-
-float speed(const control_t* control, const control_t* last_control, float deltat_t) {
-  return (control->measurement - last_control->measurement) / deltat_t;
-}
