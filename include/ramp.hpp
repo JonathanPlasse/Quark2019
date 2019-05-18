610f8f464824429cacc3cafa0ba5fbea53096dae
Jonathan Plasse <jonathan.plasse@live.fr>
2019-05-18 13:30:19 +0200

    Ramp to test

--- include/ramp.hpp
+++ include/ramp.hpp
@@ -5,14 +5,17 @@
 class Ramp {
 public:
   // Initialize Ramp class
-  Ramp(float max_speed, float max_acceleration);
+  Ramp(float max_speed, float max_acceleration, float sample_time);
 
   // Compute speed ramp
-  void compute();
+  void compute(float* delta_position);
 
 private:
+  float _filter_limit(float value, float limit);
+
   float _max_speed;
   float _max_acceleration;
+  float _sample_time;
   float _speed;
 };
 
